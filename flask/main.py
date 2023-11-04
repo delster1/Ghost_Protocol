68c8b084af06dd16b06c795bf3ae763464f6c666
Birduo <dkneidel@icloud.com>
2023-11-04 12:50:43 -0500

    Added custom naming form to Flask

--- flask/main.py
+++ flask/main.py
@@ -6,25 +6,34 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = "Deez"
-    return render_template('index.html', title=title)
+    return render_template("index.html", title=title)
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route("/uploader", methods = ["GET", "POST"])
 def upload_file():
     title = "Upload unsuccessful"
     result = title
-    if request.method == 'POST':
-        f = request.files['file']
-        print(f"Filename: {f.filename}")
+    if request.method == "POST":
+        # the filename entered in the text field to save
+        filename = request.form.get("filename")
 
-        img = face_recognition.load_image_file(f)
+        f = request.files["file"]
 
-        print(img)
+        fnamecontents = f.filename.split(".")
+
+        fext = fnamecontents[-1]
+
+        print(fext)
+
+        if fext.lower() in ["jpg", "png", "jpeg", "heic"]:
+            img = face_recognition.load_image_file(f)
+
+            print(img)
         
-        title = "Upload successful"
+            title = "Upload successful"
 
-        result = f"Upload of {f.filename} successful"
+            result = f"Upload of {f.filename} as {filename} successful"
 
-    return render_template('index.html', title=title, result=result)
+    return render_template("index.html", title=title, result=result)
 
-if __name__ == '__main__':  
+if __name__ == "__main__":  
    app.run(debug=True, port=3000)
