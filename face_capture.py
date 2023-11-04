8133e2b63ffb515c6254cdcb456223debe92b214
Jace Arnold <jace@ku.edu>
2023-11-03 22:55:03 -0500

    Add files via upload
    face capture with open cv and facial_recognition

---
+++ face_capture.py
@@ -0,0 +1,50 @@
+import face_recognition
+import cv2
+
+# Get a reference to the webcam
+video_capture = cv2.VideoCapture(0)
+
+# Reduce the resolution
+video_capture.set(3, 320)  # Width
+video_capture.set(4, 240)  # Height
+
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame, model="hog")
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for top, right, bottom, left in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
