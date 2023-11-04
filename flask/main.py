41ae943423f889ab2bfcc03abcd8e2c86c134086
Birduo <dkneidel@icloud.com>
2023-11-04 11:54:49 -0500

    Added image upload feature with numpy conversion in backend

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import face_recognition
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     title = "Deez"
+    return render_template('index.html', title=title)
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    title = "Upload unsuccessful"
+    if request.method == 'POST':
+        f = request.files['file']
+        print(f"Filename: {f.filename}")
+
+        img = face_recognition.load_image_file(f)
+
+        print(img)
+        
+        title = "Upload successful"
+
     return render_template('index.html', title=title)
 
 if __name__ == '__main__':  
