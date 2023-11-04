891c8a94d4d12aa84da8a2d31a95bd7dacb71649
Jace Arnold <jace@jrand.com>
2023-11-04 10:28:54 -0500

    Added parallel processing to hopefully increase open cv speed on other's computers

--- face_capture.py
+++ face_capture.py
@@ -7,6 +7,7 @@ import random
 import string
 from face_id import identify_faces
 from face_labels import draw_labels
+from concurrent.futures import ThreadPoolExecutor
 
 # Global constants
 WHITELISTED_FACES_DIRECTORY = "Whitelisted_Faces"
@@ -16,7 +17,8 @@ CAMERA_HEIGHT = 480
 FRAME_PROCESSING_INTERVAL = 1  # Process every frame
 RECOGNITION_TOLERANCE = 0.6  # Tolerance for face recognition
 RESIZE_FACTOR = 0.5
-RESCALE_FACTOR = 2 # doing this to avoid live division
+RESCALE_FACTOR = 2  # Doing this to avoid live division
+BATCH_SIZE = 4  # Number of faces in each batch for parallel processing
 
 blacklist_names = []
 blacklist_encodings = []
@@ -78,6 +80,18 @@ def draw_boxes(frame, face_locations, matches):
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
 
+def process_faces(face_batch):
+    # Convert each face in the batch from BGR to RGB
+    rgb_faces = [cv2.cvtColor(face, cv2.COLOR_BGR2RGB) for face in face_batch]
+    # Compute the face encodings
+    face_encodings = []
+    for face in rgb_faces:
+        encodings = face_recognition.face_encodings(face)
+        if len(encodings) > 0:
+            face_encodings.append(encodings[0])
+    return face_encodings
+
+
 def run_video(whitelist_names, whitelist_encodings):
     # Get a reference to the webcam
     video_capture = cv2.VideoCapture(0)
@@ -88,49 +102,54 @@ def run_video(whitelist_names, whitelist_encodings):
     video_capture.set(4, CAMERA_HEIGHT)
 
     frame_count = 0
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-        frame_count += 1
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        while True:
+            # Grab a single frame of video
+            ret, frame = video_capture.read()
+            frame_count += 1
 
-        # Only process every nth frame to improve performance
-        if frame_count % FRAME_PROCESSING_INTERVAL == 0:
-            # Resize frame for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
+            # Only process every nth frame to improve performance
+            if frame_count % FRAME_PROCESSING_INTERVAL == 0:
+                # Resize frame for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+                # Find all the faces in the current frame of video
+                face_locations = face_recognition.face_locations(small_frame, model="hog")
+                
+                # Prepare batches of faces for parallel processing
+                face_batches = [small_frame[top:bottom, left:right] for top, right, bottom, left in face_locations]
+                face_batches = [face_batches[i:i + BATCH_SIZE] for i in range(0, len(face_batches), BATCH_SIZE)]
+                
+                # Process each batch in parallel
+                future_results = [executor.submit(process_faces, batch) for batch in face_batches]
+                face_encodings = [enc for future in future_results for enc in future.result()]
+                
+                # Identify faces
+                identified_names = identify_faces(whitelist_names, whitelist_encodings, face_encodings)
 
-            # Find all the faces in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame, model="hog")
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-            
-            # Identify faces
-            identified_names = identify_faces(whitelist_names, whitelist_encodings, face_encodings)
+                # Compute match results
+                match_results = [name != "Unknown" for name in identified_names]
 
-            # Compute match results
-            match_results = [name != "Unknown" for name in identified_names]
+                # Draw boxes around faces
+                draw_boxes(frame, face_locations, match_results)
 
-            # Draw boxes around faces
-            draw_boxes(frame, face_locations, match_results)
+                # Identify blacklisted faces
+                for i, (face_encoding, name) in enumerate(zip(face_encodings, identified_names)):
+                    if name == "Unknown":
+                        unique_code = save_blacklisted_face(face_encoding)
+                        blacklist_names.append(unique_code)
+                        blacklist_encodings.append(face_encoding)
+                        identified_names[i] = unique_code  # Update the name in the list
 
-            # Identify blacklisted faces
-            for i, (face_encoding, name) in enumerate(zip(face_encodings, identified_names)):
-                if name == "Unknown":
-                    unique_code = save_blacklisted_face(face_encoding)
-                    blacklist_names.append(unique_code)
-                    blacklist_encodings.append(face_encoding)
-                    identified_names[i] = unique_code  # Update the name in the list
+                # Draw labels above the boxes
+                draw_labels(frame, face_locations, identified_names)
 
-            # Draw labels above the boxes
-            draw_labels(frame, face_locations, identified_names)
+            # Display the resulting image
+            cv2.imshow('Video', frame)
 
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     # Release handle to the webcam
     video_capture.release()
