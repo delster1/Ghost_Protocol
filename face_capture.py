978640d640c2dd2f72ed0e3e21ea98d62d3f7ea9
Jace Arnold <jace@jrand.com>
2023-11-04 09:58:36 -0500

    identifies non whitelisted faces, generates a unique ID for the face and saves it to the blacklist.

--- face_capture.py
+++ face_capture.py
@@ -1,19 +1,24 @@
-from PIL import Image
 import os
-import time 
+import time
 import numpy as np
 import face_recognition
 import cv2
+import random
+import string
 from face_boxes import draw_boxes
 from face_id import identify_faces
 from face_labels import draw_labels
 
-
 # Global constants
 WHITELISTED_FACES_DIRECTORY = "Whitelisted_Faces"
+BLACKLISTED_FACES_DIRECTORY = "Blacklisted_Faces"
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
-FRAME_PROCESSING_INTERVAL = 1  # Process every 2nd frame
+FRAME_PROCESSING_INTERVAL = 1  # Process every frame
+RECOGNITION_TOLERANCE = 0.6  # Tolerance for face recognition
+
+blacklist_names = []
+blacklist_encodings = []
 
 def build_whitelist():
     whitelist_names = []
@@ -29,7 +34,26 @@ def build_whitelist():
         whitelist_encodings.append(loaded_array)
 
     return whitelist_names, whitelist_encodings
+
+def generate_unique_code(length=7):
+    # Generate a random mix of letters and numbers
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def save_blacklisted_face(face_encoding, directory=BLACKLISTED_FACES_DIRECTORY):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Check if the face is already in the blacklist
+    matches = face_recognition.compare_faces(blacklist_encodings, face_encoding, tolerance=RECOGNITION_TOLERANCE)
+    if True in matches:
+        match_index = matches.index(True)
+        return blacklist_names[match_index]  # Return the existing name if face is already blacklisted
     
+    unique_code = generate_unique_code()  # Generate a unique code
+    filepath = os.path.join(directory, f"{unique_code}.npy")
+    np.save(filepath, face_encoding)
+    return unique_code
+
 def run_video(whitelist_names, whitelist_encodings):
     # Get a reference to the webcam
     video_capture = cv2.VideoCapture(0)
@@ -65,6 +89,14 @@ def run_video(whitelist_names, whitelist_encodings):
 
             # Draw boxes around faces
             draw_boxes(frame, face_locations, match_results)
+
+            # Identify blacklisted faces
+            for i, (face_encoding, name) in enumerate(zip(face_encodings, identified_names)):
+                if name == "Unknown":
+                    unique_code = save_blacklisted_face(face_encoding)
+                    blacklist_names.append(unique_code)
+                    blacklist_encodings.append(face_encoding)
+                    identified_names[i] = unique_code  # Update the name in the list
 
             # Draw labels above the boxes
             draw_labels(frame, face_locations, identified_names)
