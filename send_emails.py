c484f50446fbf1c7e52fae37fadfe760ab3524e9
Jace Arnold <jace@jrand.com>
2023-11-04 23:57:05 -0500

    Changed send_emails.py into callable

--- send_emails.py
+++ send_emails.py
@@ -3,29 +3,36 @@ import smtplib
 from creds import exec_emails  # Replace with your actual file and variable
 from location_details import location_details_string, get_location_details, city  # Replace with your actual file and variable
 
-# Gmail credentials
-gmail_user = 'jacearnoldmail@gmail.com'  # Replace with your actual email
-gmail_password = os.environ.get('GMAIL_APP_PASSWORD')  # Replace with your actual password
+def send_location_emails():
+    try:
+        # Gmail credentials
+        gmail_user = 'jacearnoldmail@gmail.com'  # Replace with your actual email
+        gmail_password = os.environ.get('GMAIL_APP_PASSWORD')  # Replace with your actual password
 
-# Email content
-location = get_location_details()
-subject = f'{city(location)}' #NEED TO CHANGE TO USE LOCATION DETAILS
+        # Email content
+        location = get_location_details()
+        subject = f'BLACKLISTER in {city(location)}'  # Using location details for the subject
 
-body = location_details_string(location)
+        body = location_details_string(location)
 
-# Set up the SMTP server
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.ehlo()
-server.login(gmail_user, gmail_password)
+        # Set up the SMTP server
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
 
-# Send the email
-for user in exec_emails:
-    to = user
-    email_text = f"From: {gmail_user}\nTo: {to}\nSubject: {subject}\n\n{body}"
-    server.sendmail(gmail_user, to, email_text)
+        # Send the email
+        for user in exec_emails:
+            to = user
+            email_text = f"From: {gmail_user}\nTo: {to}\nSubject: {subject}\n\n{body}"
+            server.sendmail(gmail_user, to, email_text)
 
+        # Close the server
+        server.close()
+        print("Emails sent successfully.")
 
-# Close the server
-server.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-print(subject)
+# If this script is run directly, call the function
+if __name__ == "__main__":
+    send_location_emails()
